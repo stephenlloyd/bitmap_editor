e8051dc528383da50477d874067714838e91162c
Stephen Lloyd <stephenlloyd@Stephens-MacBook-Pro-2.local>
2016-04-28 11:57:31 +0100

    negative canvas size test

--- spec/canvas_spec.rb
+++ spec/canvas_spec.rb
@@ -5,15 +5,20 @@ describe Canvas do
   let(:subject){described_class.new({x: 3, y: 3})}
 
   it "creates a blank canvas with default colour" do
-    expect(Canvas.new.board).to eq [[Canvas::DEFAULT_COLOUR]]
+    expect(described_class.new.board).to eq [[Canvas::DEFAULT_COLOUR]]
   end
 
   it "can't create a grid outside of its max size for x axis" do
-    expect{Canvas.new({x: Canvas::MAX_SIZE + 1, y: 1})}.to raise_error(CanvasError, "Too Big: The Canvas can only be a maximum of 250 by 250")
+    expect{described_class.new({x: Canvas::MAX_SIZE + 1, y: 1})}.to raise_error(CanvasError, "Too Big: The Canvas can only be a maximum of 250 by 250")
+  end
+
+  it "can create a negative canvas" do
+    expect(described_class.new({x: 3, y: 3}).board).to eq([["O", "O", "O"], ["O", "O", "O"], ["O", "O", "O"]])
+
   end
 
   it "can't create a grid outside of its max size for x axis" do
-    expect{Canvas.new({y: Canvas::MAX_SIZE + 1, x: 1})}.to raise_error(CanvasError, "Too Big: The Canvas can only be a maximum of 250 by 250")
+    expect{described_class.new({y: Canvas::MAX_SIZE + 1, x: 1})}.to raise_error(CanvasError, "Too Big: The Canvas can only be a maximum of 250 by 250")
   end
 
   it "fills a square within the boundaries " do
@@ -24,7 +29,6 @@ describe Canvas do
   it "errors when outside the boundaries" do
     expect{subject.fill(5, 0, "W") }.to raise_error(CanvasError, "Out of boundaries: You are trying to paint off the canvas")
   end
-
 
   it "draws a vertical line" do
     subject.vertical_line(0, 1,  2, "W")
@@ -46,4 +50,5 @@ describe Canvas do
     expect(STDOUT).to receive(:puts).with("[\"O\", \"O\", \"O\"]").exactly(3).times
     subject.show
   end
+
 end
