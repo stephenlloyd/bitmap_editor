dcd1b52a976dbceae0bd3157e121d1b2360e75fc
Stephen Lloyd <stephenlloyd@Stephens-MacBook-Pro-2.local>
2016-04-28 13:51:03 +0100

    errors folder added

---
+++ spec/errors/errors_spec.rb
@@ -0,0 +1,12 @@
+require 'errors/canvas_error'
+
+describe CanvasError do
+  it("can raise a too big error") do
+    expect(described_class.too_big.message).to eq "Too Big: The Canvas can only be a maximum of 250 by 250"
+  end
+
+  it("can raise an out of boundaries error") do
+    expect(described_class.out_of_boundaries.message).to eq "Out of boundaries: You are trying to paint off the canvas"
+  end
+
+end
